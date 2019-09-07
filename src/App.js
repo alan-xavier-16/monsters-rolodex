261315992cb2af4cfced3d8706850fcfeea8c367
Alan Xavier <alan_xvr@hotmail.com>
2019-09-07 18:46:48 -0400

    Initialize Monsters-Rolodex App - Create a class component in App.js - Use fetch API to fetch data from URL - Loop over data to generate a list of users

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monsters: []
+    };
+  }
+
+  /* Fetches users from URL below, and sets the state with the users */
+  async componentDidMount() {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let users = await response.json();
+    this.setState({ monsters: users });
+  }
+
+  render() {
+    const { monsters } = this.state;
+    return (
+      <div className="App">
+        {monsters.map((monster, idx) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
