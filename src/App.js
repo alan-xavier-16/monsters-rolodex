ddb32285b9118a8614b22440173bbdb970a45608
Alan Xavier <alan_xvr@hotmail.com>
2019-09-07 20:17:33 -0400

    Add "initial" filtering to CardList render

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
 
@@ -18,10 +19,19 @@ class App extends Component {
   }
 
   render() {
-    const { monsters } = this.state;
+    const { monsters, searchField } = this.state;
+    /* Filtering the monsters array, if text in searchField is in the name of the monsters */
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={monsters} />
+        <input
+          type="search"
+          placeholder="Search monsters"
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
