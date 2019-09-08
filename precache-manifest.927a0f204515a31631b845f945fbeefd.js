327e84feda88a0b7ca4b2d4ff29105812c6c75d9
Alan Xavier <alan_xvr@hotmail.com>
2019-09-07 22:08:08 -0400

    Updates

---
+++ precache-manifest.927a0f204515a31631b845f945fbeefd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "095e5b1535be751b4ba7a5e2584e867e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "57973bf1ca09cc93d86c",
+    "url": "/monsters-rolodex/static/css/main.37fe3bfd.chunk.css"
+  },
+  {
+    "revision": "734e9512194e17711353",
+    "url": "/monsters-rolodex/static/js/2.d93d0039.chunk.js"
+  },
+  {
+    "revision": "57973bf1ca09cc93d86c",
+    "url": "/monsters-rolodex/static/js/main.22f31d58.chunk.js"
+  },
+  {
+    "revision": "9679b25fbb58365eb7b7",
+    "url": "/monsters-rolodex/static/js/runtime~main.c610535c.js"
+  }
+]);
